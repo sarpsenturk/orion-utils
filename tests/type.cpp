fa85e66fa517304607567ca1787056e024a7135a
sarpsenturk <sarpsenturk38@gmail.com>
2023-02-12 09:21:23 +0300

    Add missing tests for type.h

--- tests/type.cpp
+++ tests/type.cpp
@@ -2,12 +2,32 @@
 
 #include <gtest/gtest.h>
 
-TEST(TypeAnyOf, ValidType)
+TEST(Type, AnyOfValid)
 {
     static_assert(orion::any_of<int, char, bool, int, float>);
 }
 
-TEST(TypeAnyOf, InvalidType)
+TEST(Type, AnyOfInvalid)
 {
     static_assert(!orion::any_of<int, char, bool, float>);
+}
+
+TEST(Type, AllOfValid)
+{
+    static_assert(orion::all_of<int, int, int, int>);
+}
+
+TEST(Type, AllOfInvalid)
+{
+    static_assert(!orion::all_of<int, int, int, bool>);
+}
+
+TEST(Type, NotEmptyValid)
+{
+    static_assert(orion::not_empty<int>);
+}
+
+TEST(Type, NotEmptyInvalid)
+{
+    static_assert(!orion::not_empty<>);
 }
