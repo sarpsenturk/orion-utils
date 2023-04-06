bfa5d2d61435d593430961605cedadbfd910533d
sarpsenturk <sarpsenturk38@gmail.com>
2023-04-06 17:29:09 +0300

    Add ORION_EXPECTS & ORION_ENSURES assertion macros

--- include/orion-utils/assertion.h
+++ include/orion-utils/assertion.h
@@ -15,3 +15,14 @@
 #else
     #define ORION_ASSERT(condition) ((void)0)
 #endif
+
+#define ORION_CONDITION_CHECK(type, condition)                                                                       \
+    do {                                                                                                             \
+        if (!(condition)) [[unlikely]] {                                                                             \
+            std::puts(fmt::format(type " failed ({}:{} {}): {}", __FILE__, __LINE__, __func__, #condition).c_str()); \
+            std::abort();                                                                                            \
+        }                                                                                                            \
+    } while (0)
+
+#define ORION_EXPECTS(condition) ORION_CONDITION_CHECK("Pre-condition", condition)
+#define ORION_ENSURES(condition) ORION_CONDITION_CHECK("Post-condition", condition)
